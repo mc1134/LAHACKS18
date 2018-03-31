cdc81bde8918aa6d878e4c4b4226cd31188aeb49
mc1134 <j.michael.chen@gmail.com>
2018-03-31 09:55:33 -0700

    Testing analyzeInput.js

--- analyzeInput.js
+++ analyzeInput.js
@@ -7,6 +7,19 @@
 
 /*
  * Sources:
+ * https://www.htmlgoodies.com/beyond/javascript/read-text-files-using-the-javascript-filereader.html
+ * https://www.html5rocks.com/en/tutorials/file/dndfiles/
  */
 
+var filename = "input.mid"
 
+
+
+//var buf = new ArrayBuffer(16);
+//var blob = new Blob([buf]);
+
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+	console.log("Success!");
+} else {
+	alert('The File APIs are not fully supported by your browser.');
+}
